e8ad855e6873afcb0c20b7e635bac34f04193789
YangTianCi <624946274@qq.com>
2019-01-02 10:57:10 +0800

    创建项目

---
+++ CodeCreator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CodeCreator
+//
+//  Created by 杨天赐 on 2019/1/2.
+//  Copyright © 2019 yangtianci. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ CodeCreator/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  CodeCreator
+//
+//  Created by 杨天赐 on 2019/1/2.
+//  Copyright © 2019 yangtianci. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+
+
+
+
+@end
